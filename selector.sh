9b3b79ab81995f6b3ab2c3d1f5cf1dd2f951c2eb
antonfomenko0610 <antonfomenko0610@outlook.com>
2021-06-19 10:37:08 -0700

    dfsf

--- selector.sh
+++ selector.sh
@@ -7,5 +7,11 @@ then
     echo $selector
 fi
 echo $selector
-
+# iterater
+for ((a=1; a <= $selector ; a++))
+do
+   printf 'Running test %s\n' "$selector"
+   # php bin/console run:algo RANK
+   # echo profit/loss $variableused somefile.txt
+done
 
