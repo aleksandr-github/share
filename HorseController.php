d3f2ad86fdf1d8c7eb2cef321ecdf3983b3be2fb
root <cihilt@gmail.com>
2021-06-21 16:43:21 +0000

    afd

--- HorseController.php
+++ HorseController.php
@@ -25,84 +25,27 @@ class HorseController extends AbstractController
      */
     public function index(): Response
     {
-        $selector = 2;
-        $sql_horseid = "SELECT horse_id  FROM `tbl_horses`";
-        $sql_raceid = "SELECT race_id  FROM tbl_races";
+        $mysqli = $this->dbConnector->getDbConnection();
+        $sql = "SELECT * FROM `tbl_horses`";
+        $result = $mysqli->query($sql);
 
-
-        // get horse id from database
-        $result_horseid = $this->dbConnector->getDbConnection()->query($sql_horseid);
-        $horse_id = array();
-        if ($result_horseid->num_rows > 0)
-        {
-            while ($row_id = $result_horseid->fetch_assoc())
-            {
-                $horse_id[] = $row_id['horse_id'];
+        $horses = [];
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_object()) {
+                $horses[] = [
+                    'horseId' => $row->horse_id,
+                    'horseName' => $row->horse_name,
+                    'horseSlug' => $row->horse_slug,
+                    'horseLatestResults' => $row->horse_latest_results,
+                    'horseAddedOn' =>$row->added_on
+                ];
             }
         }
-
-        // get race id from database
-        $result_raceid = $this->dbConnector->getDbConnection()->query($sql_raceid);
-        $race_id = array();
-        if ($result_raceid->num_rows > 0)
-        {
-            while ($row_id = $result_raceid->fetch_assoc())
-            {
-                $race_id[] = $row_id['race_id'];
-            }
-        }
-
-        $combine = array();
-        $temp_array = array();
-        for($i = 0; $i < count($race_id); $i++){
-            $combine[] = $horse_id[$i] . 'combine' . $race_id[$i];
-        }
-
-        //get distance from database
-//        for($i = 0; $i < count($race_id); $i++){
-        for($i = 0; $i < 2; $i++){
-//            for($j = 0; $i < count($horse_id); $j++){
-            for($j = 0; $j < 2; $j++){
-                $distance = [];
-                $query = "SELECT race_distance FROM tbl_hist_results WHERE race_id=".$race_id[$i]." and horse_id=".$horse_id[$j]." GROUP BY race_distance";
-                // get race id from database
-                $result = $this->dbConnector->getDbConnection()->query($query);
-                if ($result->num_rows > 0)
-                {
-                    while ($row = $result->fetch_assoc())
-                    {
-                        $distance[] = $row['race_distance'];
-                    }
-                }
-                for($k = 0; $k < count($distance); $k++){
-                    $temp_array = [];
-                    $query = "SELECT * FROM tbl_hist_results WHERE race_id=".$race_id[$i]." and horse_id=".$horse_id[$j]." and race_distance='".$distance[$k]."' order by horse_position";
-                    // get race id from database
-                    $result = $this->dbConnector->getDbConnection()->query($query);
-                    if ($result->num_rows > 0)
-                    {
-                        while ($row = $result->fetch_object())
-                        {
-                            $temp_array[] = [
-                                'rating' => $row->rating,
-                                'rank' => $row->rank,
-                                'horse_fixed_odds' => $row->horse_fixed_odds,
-                                'position' => $row->horse_position
-                            ];
-                            if(count($temp_array) >= $selector)
-                                break;
-                        }
-                    }
-                }
-
-            }
-        }
+        $mysqli->close();
 
         return $this->render('horses.html.twig', [
-            'horses' => $horse_id,
-            'race_ids' => $race_id,
-            'combines' => $combine,
-            'temp_arrays' => $temp_array
+            'horses' => $horses
         ]);
     }
 
